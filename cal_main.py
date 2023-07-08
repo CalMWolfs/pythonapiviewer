5a2535a9d0e72a1bc08546a60e300992a3e6bdfa
CalMWolfs <cwolfson58@gmail.com>
2023-07-08 21:35:13 +1000

    using utils

--- cal_main.py
+++ cal_main.py
@@ -4,14 +4,7 @@ import json
 from secrets import API_KEY
 import constants_parsing
 from features import skills
-
-
-def printHeader(text):
-    print(f'\n-------------------------------------\n{text}\n-------------------------------------')
-
-
-def printSmallHeader(text):
-    print(f'------------{text}------------')
+from utils import text_formatting
 
 
 def fetchData():
@@ -74,14 +67,14 @@ mining_data = profile_specific.get('mining_core', {})
 with open('constants.json') as file:
     constants = json.load(file)
 
-printHeader('SkyBlock level and Skill levels')
+text_formatting.printHeader('SkyBlock level and Skill levels')
 skyblock_level = profile_specific.get('leveling', {}).get('experience', 0) / 100
 print(f'SkyBlock level: {skyblock_level}')
 
 # todo deal with api off
 skills.getSkillData(profile_specific)
 
-printHeader('Dungeons')
+text_formatting.printHeader('Dungeons')
 level, exp = constants_parsing.getCatacombsLevel(
     dungeons_data.get('dungeon_types', {}).get('catacombs', {}).get('experience', 0))
 print(f'Catacombs {level} with {exp} exp')
@@ -97,7 +90,7 @@ completed_dungeon = completed_master = True
 # todo get other stats fastest run, highest score, watcher kills etc
 for floor in range(8):
     floor_name = 'Entrance' if floor == 0 else f'Floor {floor}'
-    printSmallHeader(floor_name)
+    text_formatting.printSmallHeader(floor_name)
 
     runs = dungeons_data.get('dungeon_types', {}).get('catacombs', {}).get('tier_completions', {}).get(str(floor), 0)
     master_runs = dungeons_data.get('dungeon_types', {}).get('master_catacombs', {}).get('tier_completions', {}).get(str(floor), 0)
@@ -117,7 +110,7 @@ for floor in range(8):
     if floor != 0:
         print(f'Master {floor_name} completed {format(int(master_runs), ",")} times')
 
-printSmallHeader('Dungeons Stats')
+text_formatting.printSmallHeader('Dungeons Stats')
 if not completed_dungeon:
     print('Dungeons never completed')
 else:
@@ -129,9 +122,9 @@ else:
     print('Secrets: Will do later')
 
 # todo collection lvl and maybe say if it is maxed?
-printHeader('Collections')
+text_formatting.printHeader('Collections')
 for collection_type in constants['collections']:
-    printSmallHeader(collection_type)
+    text_formatting.printSmallHeader(collection_type)
     output = ''
     collections_dict = constants['collections'][collection_type]
     for collection in collections_dict:
@@ -140,22 +133,22 @@ for collection_type in constants['collections']:
         output += f'{display_name}: {amount} '
     print(output)
 
-printHeader('Coins')
+text_formatting.printHeader('Coins')
 print('Purse:', format(int(profile_specific.get('coin_purse', 0)), ','))
 print('Bank:', format(int(profile.get('banking', {}).get('balance', 0)), ','))
 
-printHeader('Accessories')
+text_formatting.printHeader('Accessories')
 mp = accessories_data.get('highest_magical_power', 0)
 print(f'Magical Power:', format(mp, ','))
 print(f'Selected Power:', accessories_data.get('selected_power', 'None'))
-printSmallHeader('Tuning Points')
+text_formatting.printSmallHeader('Tuning Points')
 for stat in accessories_data.get('tuning', {}).get('slot_0', {}):
     allocated = accessories_data.get('tuning', {}).get('slot_0', {}).get(stat, 0)
     if allocated != 0:
         print(f'{stat}:', allocated, 'points')
 
 # todo only mark as important based on rarity (why does hypixel not use internal names here????)
-printHeader('Pets')
+text_formatting.printHeader('Pets')
 for pet in pet_data:
     # todo sort important pets, pet candies, pet item (needs to be mapped)
     pet_exp = pet.get('exp', 0)
@@ -167,10 +160,10 @@ for pet in pet_data:
         level, exp = constants_parsing.getPetLevel(pet_rarity, pet_exp)
     print(pet_rarity, pet.get('type', ''), f'lvl {level} with {exp} exp')
 
-printHeader('Slayers')
+text_formatting.printHeader('Slayers')
 for slayer in constants['slayers']:
     slayer_name = constants['slayers'].get(slayer, slayer)
-    printSmallHeader(slayer_name)
+    text_formatting.printSmallHeader(slayer_name)
     level, exp = constants_parsing.getSlayerLevel(slayer, slayer_data.get(slayer, {}).get('xp', 0))
     if exp == '0':
         print(f'No {slayer_name} progress')
@@ -182,20 +175,20 @@ for slayer in constants['slayers']:
         print(f'Tier {tier + 1} kills:', format(kills, ','))
 
 # todo calculate nucleus runs done through placed crystals?
-printHeader('Heart Of The Mountain')
+text_formatting.printHeader('Heart Of The Mountain')
 level, exp = constants_parsing.getHotmLevel(mining_data.get('experience', 0))
 if level == 7:
     print('HOTM 7')
 else:
     print(f'HOTM {level} with {exp} exp')
 print(f'Tokens Spent:', mining_data.get('tokens_spent', 0))
-printSmallHeader('Mithril Powder')
+text_formatting.printSmallHeader('Mithril Powder')
 spent = mining_data.get('powder_spent_mithril', 0)
 available = mining_data.get('powder_mithril_total', 0)
 print('Total:', format(available + spent, ','))
 print('Available:', format(available, ','))
 print('Spent:', format(spent, ','))
-printSmallHeader('Gemstone Powder')
+text_formatting.printSmallHeader('Gemstone Powder')
 spent = mining_data.get('powder_spent_gemstone', 0)
 available = mining_data.get('powder_gemstone_total', 0)
 print('Total:', format(available + spent, ','))
@@ -203,7 +196,7 @@ print('Available:', format(available, ','))
 print('Spent:', format(spent, ','))
 
 # todo filter some of these perks
-printSmallHeader('HOTM Perks')
+text_formatting.printSmallHeader('HOTM Perks')
 ability_name = mining_data.get('selected_pickaxe_ability', 'None')
 print('Selected pickaxe ability:', constants['hotm_perks'].get(ability_name, ability_name))
 for perk in mining_data.get('nodes', {}):
